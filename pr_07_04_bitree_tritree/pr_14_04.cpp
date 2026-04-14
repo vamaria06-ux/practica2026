5511098741ef891cfe4ee7c5dfda046109b8eb31
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-04-14 11:06:39 +0300

    include BiTree

--- pr_07_04_bitree_tritree/pr_14_04.cpp
+++ pr_07_04_bitree_tritree/pr_14_04.cpp
@@ -37,3 +37,33 @@ bool isEqual(BiTree<T>* lhs, BiTree<T>* rhs, Cmp cmp)
   }
   return !hasNext(blhs) && !hasNext(brhs);
 }
+// Входит ли одно дерево в другое
+template <class T, class Cmp>
+bool included(BiTree<T> * origin, BiTree<T>* rhs) // проверяем входит лм rhs в origin, потом можно переиспользовать
+{
+  if (!rhs) return true; // если дерево пустое, то оно входит
+  if (!origin) return false;
+  BiTree<T> * brhs = fall_left(rhs);
+  BiTree<T>* b = find(origin, brhs -> val, cmp);
+  return isEqualRange(brhs, brhs -> val,b, cmp);
+}
+template <class T,class Cmp>
+BiTree<T> * find (BiTree<T> * r, T val, Cmp cmp);
+template<class T, class Cmp>
+bool isEqual (BiTree<T>* lhs, BiTree<T>* rhs, Cmp cmp)
+{
+  BiTree<T> * b1 = fall_left(lhs);
+  BiTree<T> * b2 = fall_legt(rhs);
+  return isEqualRange(b1, nullptr, b2, cmp);
+}
+template <class T, class Cmp>
+bool isEqualRange (BiTree<T> * b1, BiTree<T> * e1, BiTree<T> * b2, Cmp cmp)
+{
+  while (b1 != e1 && hasNext(b2))
+  {
+    if (!equal(b1 -> val,b2 -> val,cmp)) return false;
+    b1 = next(b1);
+    b2 = next(b2);
+  }
+  return (b1 == e1);
+}
